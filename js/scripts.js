24481da4fdf16583de34c3e97b8f617b225eb6ff
Dylan Grech <dylangrech99@gmail.com>
2022-02-12 16:27:21 +0100

    New Object
    If applied this will add a new object to test out if the loop is working correctly as well as fix small issues to make the code cleaner.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
     {
-        name: 'Bublbasaur',
+        name: 'Bulbasaur',
         height: 0.7,
         weight: 6.9,
         type: ['grass', 'poison']
@@ -9,22 +9,28 @@ let pokemonList = [
         name: 'Charmander',
         height: 0.6,
         weight: 8.5,
-        type: ['Fire']
+        type: ['fire']
     },
     {
         name: 'Squirtle',
         height: 0.5,
         weight: 9,
         type: ['Water']
+    },
+    {  
+        name: 'Charizard',
+        height: 1.7,
+        weight: 90.5,
+        type: ['fire', 'flying']
     }
 ]
 for (let i=0; i < pokemonList.length; i++){
     if (pokemonList[i].height <1.0 && pokemonList[i].height >= 0.6){
       document.write(pokemonList[i].name + " " + pokemonList[i].height + '<br>');
     }else if (pokemonList[i].height <=0.5){
-      document.write(pokemonList[i].name + " " + pokemonList[i].height + " Now that is small. " + '<br>');
+      document.write(pokemonList[i].name + " " + pokemonList[i].height + " - Now that is small. " + '<br>');
     }
     else if (pokemonList[i].height >1.0){
-        document.write(pokemonList[i].name + " " + pokemonList[i].height + " That is big. " + '<br>')
+        document.write(pokemonList[i].name + " " + pokemonList[i].height + " - That is big. " + '<br>')
     }
   }
