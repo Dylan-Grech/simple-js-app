ca0c21c589de3b6578c225d69aed327f63a52ef2
Dylan Grech <dylangrech99@gmail.com>
2022-02-14 19:00:35 +0100

    Added Function
    If applied this will add a function.

--- js/scripts.js
+++ js/scripts.js
@@ -24,13 +24,17 @@ let pokemonList = [
         type: ['fire', 'flying']
     }
 ]
-for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height <1.0 && pokemonList[i].height >= 0.6){
-      document.write(pokemonList[i].name + " " + pokemonList[i].height + '<br>');
-    }else if (pokemonList[i].height <=0.5){
-      document.write(pokemonList[i].name + " " + pokemonList[i].height + " - Now that is small. " + '<br>');
+function printPokemonDetails(){
+    for (let i=0; i < pokemonList.length; i++){
+        if (pokemonList[i].height <1.0 && pokemonList[i].height >= 0.6){
+        document.write(pokemonList[i].name + " " + pokemonList[i].height + '<br>');
+        }else if (pokemonList[i].height <=0.5){
+        document.write(pokemonList[i].name + " " + pokemonList[i].height + " - Now that is small. " + '<br>');
+        }
+        else if (pokemonList[i].height >1.0){
+            document.write(pokemonList[i].name + " " + pokemonList[i].height + " - That is big. " + '<br>')
+        }
     }
-    else if (pokemonList[i].height >1.0){
-        document.write(pokemonList[i].name + " " + pokemonList[i].height + " - That is big. " + '<br>')
-    }
-  }
+}
+
+printPokemonDetails();
