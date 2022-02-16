b1eeccceedc772481aa709dffe95d00c32cbe34b
Dylan Grech <dylangrech99@gmail.com>
2022-02-16 19:39:00 +0100

    IIFE
    If applied, this will add an IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,20 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        weight: 6.9,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        weight: 8.5,
-        type: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        weight: 9,
-        type: ['Water']
-    },
-    {  
-        name: 'Charizard',
-        height: 1.7,
-        weight: 90.5,
-        type: ['fire', 'flying']
+let pokemonRepository = (function () {
+    let pokemonList = [];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
     }
-]
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+  
 undefined
 pokemonList.forEach(function(pokemon) {
 	if (pokemon.height <1.0 && pokemon.height >= 0.6){
