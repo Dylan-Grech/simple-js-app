fa845c533be9d24fb476047976cc211df45b233b
Dylan Grech <dylangrech99@gmail.com>
2022-02-16 19:45:21 +0100

    Update to pokemonList and function.
    If applied this will add objects in pokemonList and define the function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 let pokemonRepository = (function () {
-    let pokemonList = [];
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            weight: 6.9,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            weight: 8.5,
+            type: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            weight: 9,
+            type: ['Water']
+        },
+        {  
+            name: 'Charizard',
+            height: 1.7,
+            weight: 90.5,
+            type: ['fire', 'flying']
+        }
+    ];
   
     function add(pokemon) {
       pokemonList.push(pokemon);
@@ -14,15 +39,16 @@ let pokemonRepository = (function () {
       getAll: getAll
     };
   })();
-  
-undefined
-pokemonList.forEach(function(pokemon) {
-	if (pokemon.height <1.0 && pokemon.height >= 0.6){
-        document.write(pokemon.name + " " + pokemon.height + '<br>');
-        }else if (pokemon.height <=0.5){
-        document.write(pokemon.name + " " + pokemon.height + " - Now that is small. " + '<br>');
-        }
-        else if (pokemon.height >1.0){
-            document.write(pokemon.name + " " + pokemon.height + " - That is big. " + '<br>')
-        }
-})
+
+function printPokemonDetails(){
+    pokemonList.forEach(function(pokemon) {
+        if (pokemon.height <1.0 && pokemon.height >= 0.6){
+            document.write(pokemon.name + " " + pokemon.height + '<br>');
+            }else if (pokemon.height <=0.5){
+            document.write(pokemon.name + " " + pokemon.height + " - Now that is small. " + '<br>');
+            }
+            else if (pokemon.height >1.0){
+                document.write(pokemon.name + " " + pokemon.height + " - That is big. " + '<br>')
+            }
+    })
+}
